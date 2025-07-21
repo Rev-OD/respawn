03a576a1e3aa85d6d51d317b6d63dfc36081dd1b
Rev-OD <dertdurt37@gmail.com>
2025-07-21 17:44:11 +0300

    Update respawn.lua

--- respawn.lua
+++ respawn.lua
@@ -6,19 +6,15 @@ local imgui = require 'imgui'
 
 function main()
     repeat wait(0) until isSampAvailable()
+
     
     sampRegisterChatCommand("respawn", function(arg)
         local id = tonumber(arg:match("%d+"))
         local x, y, z = getCharCoordinates(PLAYER_PED)
         local world = 0
-        local reconInfo = {
-            ['status'] = imgui.ImBool(false),
-        }
-        if not reconInfo['status'].v then
-            z = z
-        else
-            z = z - 19
-        end
+        
+
+
         
         local function handleWorldMessage(color, text)
             if text:find("Current world:") then
@@ -30,6 +26,39 @@ function main()
         sampev.onServerMessage = handleWorldMessage
 
         interior = getCharActiveInterior(PLAYER_PED)
+
+        z = z - 19
+        
+        sampSendChat("/setvw")
+        sampSendChat("/spplayer " .. id)    
+        
+        lua_thread.create(function()
+            wait(500)
+            sampSendChat("/plpos " .. id .. " " .. x .. " " .. y .. " " .. z .. " " .. world .. " " .. interior)
+            wait(100)
+            sampev.onServerMessage = nil
+        end)
+    end)
+
+    sampRegisterChatCommand("respa", function(arg)
+        local id = tonumber(arg:match("%d+"))
+        local x, y, z = getCharCoordinates(PLAYER_PED)
+        local world = 0
+        
+
+
+        
+        local function handleWorldMessage(color, text)
+            if text:find("Current world:") then
+                world = text:match("Current world:%s*(%d+)")
+                return false
+            end
+        end
+        
+        sampev.onServerMessage = handleWorldMessage
+
+        interior = getCharActiveInterior(PLAYER_PED)
+
         
         sampSendChat("/setvw")
         sampSendChat("/spplayer " .. id)    
